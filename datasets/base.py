1ccad3ef30524146da44f6bc44e68ba40db73bc2
navidasj96 <89703564+navidasj96@users.noreply.github.com>
2022-12-23 09:40:24 +0330

    Update base.py

--- datasets/base.py
+++ datasets/base.py
@@ -62,51 +62,7 @@ class BaseDataset(data.Dataset):
     def dec_evaluation(self, result_path):
         return None
 
-    def data_transform(self, image, annotation):
-        # only do random_flip augmentation to original images
-        crop_size = None
-        crop_center = None
-        crop_size, crop_center = random_crop_info(h=image.shape[0], w=image.shape[1])
-        image, annotation['pts'], crop_center = random_flip(image, annotation['pts'], crop_center)
-        if crop_center is None:
-            crop_center = np.asarray([float(image.shape[1])/2, float(image.shape[0])/2], dtype=np.float32)
-        if crop_size is None:
-            crop_size = [max(image.shape[1], image.shape[0]), max(image.shape[1], image.shape[0])]  # init
-        M = load_affine_matrix(crop_center=crop_center,
-                               crop_size=crop_size,
-                               dst_size=(self.input_w, self.input_h),
-                               inverse=False,
-                               rotation=True)
-        image = cv2.warpAffine(src=image, M=M, dsize=(self.input_w, self.input_h), flags=cv2.INTER_LINEAR)
-        if annotation['pts'].shape[0]:
-            annotation['pts'] = np.concatenate([annotation['pts'], np.ones((annotation['pts'].shape[0], annotation['pts'].shape[1], 1))], axis=2)
-            annotation['pts'] = np.matmul(annotation['pts'], np.transpose(M))
-            annotation['pts'] = np.asarray(annotation['pts'], np.float32)
-
-        out_annotations = {}
-        size_thresh = 3
-        out_rects = []
-        out_cat = []
-        for pt_old, cat in zip(annotation['pts'] , annotation['cat']):
-            if (pt_old<0).any() or (pt_old[:,0]>self.input_w-1).any() or (pt_old[:,1]>self.input_h-1).any():
-                pt_new = pt_old.copy()
-                pt_new[:,0] = np.minimum(np.maximum(pt_new[:,0], 0.), self.input_w - 1)
-                pt_new[:,1] = np.minimum(np.maximum(pt_new[:,1], 0.), self.input_h - 1)
-                iou = ex_box_jaccard(pt_old.copy(), pt_new.copy())
-                if iou>0.6:
-                    rect = cv2.minAreaRect(pt_new/self.down_ratio)
-                    if rect[1][0]>size_thresh and rect[1][1]>size_thresh:
-                        out_rects.append([rect[0][0], rect[0][1], rect[1][0], rect[1][1], rect[2]])
-                        out_cat.append(cat)
-            else:
-                rect = cv2.minAreaRect(pt_old/self.down_ratio)
-                if rect[1][0]<size_thresh and rect[1][1]<size_thresh:
-                    continue
-                out_rects.append([rect[0][0], rect[0][1], rect[1][0], rect[1][1], rect[2]])
-                out_cat.append(cat)
-        out_annotations['rect'] = np.asarray(out_rects, np.float32)
-        out_annotations['cat'] = np.asarray(out_cat, np.uint8)
-        return image, out_annotations
+    
 
     def __len__(self):
         return len(self.img_ids)
@@ -157,18 +113,18 @@ class BaseDataset(data.Dataset):
         image = np.asarray(np.clip(image, a_min=0., a_max=255.), np.float32)
         image = np.transpose(image / 255. - 0.5, (2, 0, 1))
 
-        image_h = self.input_h // self.down_ratio
-        image_w = self.input_w // self.down_ratio
+        image_h = 800 // 4
+        image_w = 800 // 4
 
-        hm = np.zeros((self.num_classes, image_h, image_w), dtype=np.float32)
-        wh = np.zeros((self.max_objs, 10), dtype=np.float32)
+        hm = np.zeros((2, image_h, image_w), dtype=np.float32)
+        wh = np.zeros((500, 10), dtype=np.float32)
         ## add
-        cls_theta = np.zeros((self.max_objs, 1), dtype=np.float32)
+        cls_theta = np.zeros((500, 1), dtype=np.float32)
         ## add end
-        reg = np.zeros((self.max_objs, 2), dtype=np.float32)
-        ind = np.zeros((self.max_objs), dtype=np.int64)
-        reg_mask = np.zeros((self.max_objs), dtype=np.uint8)
-        num_objs = min(annotation['rect'].shape[0], self.max_objs)
+        reg = np.zeros((500, 2), dtype=np.float32)
+        ind = np.zeros((500), dtype=np.int64)
+        reg_mask = np.zeros((500), dtype=np.uint8)
+        num_objs = min(annotation['rect'].shape[0], 500)
         # ###################################### view Images #######################################
         # copy_image1 = cv2.resize(image, (image_w, image_h))
         # copy_image2 = copy_image1.copy()
@@ -265,8 +221,6 @@ class BaseDataset(data.Dataset):
 
         elif self.phase == 'train':
             annotation = self.load_annotation(index)
-            image, annotation = self.data_transform(image, annotation)
             data_dict = self.generate_ground_truth(image, annotation)
             return data_dict
-
 
