3172ca04c4baff1826f59be7c756f2e21f5b6867
navidasj96 <89703564+navidasj96@users.noreply.github.com>
2022-12-23 10:14:20 +0330

    Update ctrbox_net.py

--- models/ctrbox_net.py
+++ models/ctrbox_net.py
@@ -10,7 +10,7 @@ class CTRBOX(nn.Module):
         channels = [3, 64, 256, 512, 1024, 2048]
         assert down_ratio in [2, 4, 8, 16]
         self.l1 = int(np.log2(down_ratio))
-        self.base_network = resnet.resnet101(pretrained=pretrained)
+        self.base_network = resnet.res2net50(pretrained=pretrained)
         self.dec_c2 = CombinationModule(512, 256, batch_norm=True)
         self.dec_c3 = CombinationModule(1024, 512, batch_norm=True)
         self.dec_c4 = CombinationModule(2048, 1024, batch_norm=True)
