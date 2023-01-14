6911ae4482a2bed8b3bbec5988960fb8cb98d5c1
navidasj96 <89703564+navidasj96@users.noreply.github.com>
2023-01-14 12:33:09 +0330

    Update dataset_dota.py

--- datasets/dataset_dota.py
+++ datasets/dataset_dota.py
@@ -7,12 +7,37 @@ import pickle
 class DOTA(BaseDataset):
     def __init__(self, data_dir, phase, input_h=None, input_w=None, down_ratio=None):
         super(DOTA, self).__init__(data_dir, phase, input_h, input_w, down_ratio)
-        self.category = [
+        self.category = ['plane',
+                         'baseball-diamond',
+                         'bridge',
+                         'ground-track-field',
                          'small-vehicle',
-                         'large-vehicle'
+                         'large-vehicle',
+                         'ship',
+                         'tennis-court',
+                         'basketball-court',
+                         'storage-tank',
+                         'soccer-ball-field',
+                         'roundabout',
+                         'harbor',
+                         'swimming-pool',
+                         'helicopter'
                          ]
         self.color_pans = [(204,78,210),
-                           (0,192,255)]
+                           (0,192,255),
+                           (0,131,0),
+                           (240,176,0),
+                           (254,100,38),
+                           (0,0,255),
+                           (182,117,46),
+                           (185,60,129),
+                           (204,153,255),
+                           (80,208,146),
+                           (0,0,204),
+                           (17,90,197),
+                           (0,255,255),
+                           (102,255,102),
+                           (255,255,0)]
         self.num_classes = len(self.category)
         self.cat_ids = {cat:i for i,cat in enumerate(self.category)}
         self.img_ids = self.load_img_ids()
